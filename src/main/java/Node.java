316c402ad2a82e636cce75617d9ba827086963f3
oksana <kharabara99@gmail.com>
2021-09-14 19:19:36 +0300

    Added tasks for streams

---
+++ src/main/java/Node.java
@@ -0,0 +1,18 @@
+public class Node {
+    private int index;
+    private String name;
+
+    public Node(int index, String name) {
+        this.index = index;
+        this.name = name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
